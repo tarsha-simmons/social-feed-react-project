f1b85d2ff6630a8e9646efba163845bbeb3c2766
Tarsha Simmons <tarsha.simmons04@gmail.com>
2022-05-02 14:23:44 -0500

    deleted unneccesary files, imported react state hook into app.js, starting on visuals

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+ 
+  const now = Date()
+  const [posts, setPosts] = useState([{username: "Tarsha Simmon," postBody: "I can't wait try a new recipe tonight.", timestamp: now}])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <h3> Hello World </h3>
     </div>
   );
 }
