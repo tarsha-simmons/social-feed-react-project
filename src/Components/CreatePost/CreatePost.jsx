25c9f8f37b014a61df496a7afcc5c2de10a38c2f
Tarsha Simmons <tarsha.simmons04@gmail.com>
2022-05-02 17:11:37 -0500

    edoted create posts and display posts, need to find charger, in case laptop goes down also so changeds are saved for pull

--- src/Components/CreatePost/CreatePost.jsx
+++ src/Components/CreatePost/CreatePost.jsx
@@ -12,17 +12,21 @@ const CreatePost = (props) => {
         postBody: postBody,
         timestamp: Date()
         }
-        props.addNewPost(newPost)
+        props.addNewPost(newPost);
     }
+    return (
+        <div>
     <form onSubmit={handleSubmit}>
-    <div className='mb-3'>
+    
         <label className='form-label'>Name</label>
         <input type="text" className='form-control' value={userName} onChange={(event) => setUserName(event.target.value)}/>
         <label className='form-label'>Post</label>
         <textarea className='form-control' value={postBody} onChange={(event) => setPostBody(event.target.value)}>placeholder</textarea>
         <button className='btn btn-info' type='submit'>Create</button>
-    </div>
 </form>
+</div>
+
+    );
 }
 
     export default CreatePost;
